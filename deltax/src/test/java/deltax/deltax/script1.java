5c67b3c332400bea38ca6e40f441030804109a49
kanhu69 <33693855+kanhu69@users.noreply.github.com>
2018-07-25 20:19:25 +0530

    second pusf

---
+++ deltax/src/test/java/deltax/deltax/script1.java
@@ -0,0 +1,47 @@
+package deltax.deltax;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class script1 
+{
+	WebDriver driver;
+
+	/**
+	 * This function will execute before each Test tag in testng.xml
+	 * @param browser
+	 * @throws Exception
+	 */
+	@BeforeTest
+	@Parameters("browser")
+	public void setup(String browser) throws Exception{
+		//Check if parameter passed from TestNG is 'firefox'
+		if(browser.equalsIgnoreCase("firefox")){
+		//create firefox instance
+			System.setProperty("webdriver.firefox.marionette", ".\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		//Check if parameter passed as 'chrome'
+		else if(browser.equalsIgnoreCase("chrome")){
+			//set path to chromedriver.exe
+			System.setProperty("webdriver.chrome.driver",".\\chromedriver.exe");
+			//create chrome instance
+			driver = new ChromeDriver();
+		}
+	
+			
+		}
+		
+	
+	
+	@Test
+	public void Run1()
+	{
+		
+	}
+
+}
